0d8eff24ceb660dd76188c1cdf70227e90c9a8d3
Chenyang Shi <cs3000@columbia.edu>
2019-07-17 09:25:48 -0500

    Update selection_quick_merge_bubble_sort.py
    small edits on bubble sort.

--- Useful_Code_Snippets/selection_quick_merge_bubble_sort.py
+++ Useful_Code_Snippets/selection_quick_merge_bubble_sort.py
@@ -56,10 +56,10 @@ class Solution:
     def bubble_sort(self, nums):
         for i in range(len(nums)):
             for j in range(len(nums) - 1 - i):
-                if nums[j] < nums[j+1]:
+                if nums[j] > nums[j+1]:
                     nums[j], nums[j+1] = nums[j+1], nums[j]
                 
-        return nums[::-1]
+        return nums
 
 class Test(unittest.TestCase):
     def __init__(self, *args, **kwargs):
