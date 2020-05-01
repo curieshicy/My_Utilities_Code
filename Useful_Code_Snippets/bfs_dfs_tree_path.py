33439574deefadec6c3ea7afe467f37d5c45fcb4
Chenyang Shi <cs3000@columbia.edu>
2020-05-01 16:27:04 -0500

    Update bfs_dfs_tree_path.py

--- Useful_Code_Snippets/bfs_dfs_tree_path.py
+++ Useful_Code_Snippets/bfs_dfs_tree_path.py
@@ -4,19 +4,19 @@ class Solution:
             return []
 
         from collections import deque
-        queue = deque([(root, str(root.val))])
+        queue = deque([root])
         res = []
         while queue:
             level_path = ''
             for i in range(len(queue)):
-                node, path = queue.popleft()
-                level_path += path
+                node = queue.popleft()
+                level_path += str(node.val)
                 
                 if node.left:
-                    queue.append((node.left, str(node.left.val)))
+                    queue.append(node.left)
 
                 if node.right:
-                    queue.append((node.right, str(node.right.val)))
+                    queue.append(node.right)
 
             res.append(level_path)
         return res
