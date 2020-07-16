29f6475ee87950213014b8e8f7cc3fba273cd73f
Chenyang Shi <cs3000@columbia.edu>
2020-07-16 02:29:24 -0500

    Update topological_sorting.py

--- Useful_Code_Snippets/topological_sorting.py
+++ Useful_Code_Snippets/topological_sorting.py
@@ -1,10 +1,10 @@
 from collections import defaultdict
-edges = [[0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 4]]
+edges = [[1, 0], [1, 2], [2, 5], [0, 3], [3, 4], [3, 7], [4, 6]]
 graph = defaultdict(list)
 for start, end in edges:
     graph[start].append(end)
 
-def topo_sort(graph, n, pre, post):
+def topo_sort(graph, n, start, pre, post):
     def explore(u):
         nonlocal clock
         pre[u] = clock
@@ -18,9 +18,10 @@ def topo_sort(graph, n, pre, post):
         
     clock = 1
     visited = [False for i in range(n)]
-    for node in range(n):
-        if visited[node] == False:
-            explore(node)
+    vertices = [start] + list(set(range(n)) - {start})
+    for vertex in vertices:
+        if visited[vertex] == False:
+            explore(vertex)
 
     # detect back edge == detect cycle
     is_cycle = False
@@ -33,7 +34,5 @@ def topo_sort(graph, n, pre, post):
     sorted_keys = sorted(post.keys(), key = post.get, reverse = True)
     return sorted_keys, pre, post, is_cycle
         
-print (topo_sort(graph, 8, dict(), dict()))
 
-
-
+print (topo_sort(graph, 8, 1, dict(), dict()))
